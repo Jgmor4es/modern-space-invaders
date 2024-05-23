7478f738a4f4d2365dac4d06bca9cf9e478dc0df
Jgmor4es <jgmor4es@gmail.com>
2024-05-23 13:38:26 -0400

    add Grid class

--- js/classes/Bomb.js
+++ js/classes/Bomb.js
@@ -56,14 +56,14 @@ class Bomb {
   }
 }
 
-class PowerUp() {
-    constructor({ position, velocity }) {
+class PowerUp(){
+    constructor({ position, velocity }); {
         this.position = position;
         this.velocity = velocity;
         this.radius = 15;
     }
 
-    draw() {
+    draw(); {
         c.beginPath();
         c.arc(this.position.x, this.position.y, this.radius, 0, Math.PI * 2);
         c.fillStyle = "yellow";
@@ -71,7 +71,7 @@ class PowerUp() {
         c.closePath();
     }
 
-    update() {
+    update(); {
         this.draw();
         this.position.x += this.velocity.x;
         this.position.y += this.velocity.y;
