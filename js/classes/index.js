f43e8b36d536ed6f2e014be01cc1db7a5f992254
Jgmor4es <jgmor4es@gmail.com>
2024-06-06 14:45:53 -0400

    add endGame function in index.js file

--- js/classes/index.js
+++ js/classes/index.js
@@ -89,3 +89,23 @@ function init() {
         );
     }
 }
+
+function endGame() {
+    audio.gameOver.play();
+
+    setTimeout(() => {
+        player.opacity = 0;
+        game.over = true;
+    }, 0);
+
+    setTimeout(() => {
+        game.active = false;
+        doccument.querySelector("#restartScreen").style.display = "flex";
+    }, 2000);
+
+    createParticles({
+        object: player,
+        colors: "white",
+        fades: true
+    });
+}
